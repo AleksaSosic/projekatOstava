f5f1cf7994026b07c68efa07680fd63b9aee77c3
Aleksa Sosic <aleksa2005sosic@gmail.com>
2023-03-15 23:20:38 +0100

    Add project files.

---
+++ WebApplication1/Controllers/ostavaController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Konekcija;
+using System.Security.Principal;
+using System.Net.Http;
+using System.Web.UI;
+
+namespace WebApplication1.Controllers
+{
+    public class ostavaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Unesi()
+        {
+            return View();
+        }
+        public ActionResult SacuvajPodatke(ostava ost)
+        {
+            using (SqlConnection konekt = new SqlConnection(Konekcija.Konekcija.KonektujSe()))
+            {
+                using (SqlCommand komand = new SqlCommand("INSERT INTO ostava VALUES ('" + ost.naziv + "', '" + ost.kolicina + "', " + ost.rok_trajanja + ")", konekt))
+                {
+                    if (konekt.State != ConnectionState.Open) konekt.Open();
+                    komand.ExecuteNonQuery();
+                }
+            }
+
+            return RedirectToAction("table_ostava");
+        }
+        public ActionResult table_ostava()
+        {
+            List<ostava> ostava = new List<ostava>();
+            using (SqlConnection konekt = new SqlConnection(Konekcija.Konekcija.KonektujSe()))
+            {
+                using (SqlCommand komand = new SqlCommand("SELECT * FROM ostava", konekt))
+                {
+                    if (konekt.State != ConnectionState.Open) konekt.Open();
+
+                    SqlDataReader dr_ostava = komand.ExecuteReader();
+                    DataTable dt_ostava = new DataTable();
+                    dt_ostava.Load(dr_ostava);
+
+                    foreach (DataRow ostavaSlog in dt_ostava.Rows) ostava.Add(new ostava(ostavaSlog));
+                }
+            }
+
+            return View(ostava);
+        }
+    }
+}
---
+++ WebApplication1/Models/ostava.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WebApplication1.Models
+{
+    public class ostava
+    {
+        public int id { get; set; }
+        public string naziv { get; set; }
+        public int kolicina { get; set; }
+        public string rok_trajanja { get; set; }
+
+        public ostava() { }
+
+        public ostava(DataRow ostava)
+        {
+            id = (int)ostava["id"];
+            naziv = ostava["naziv"].ToString();
+            kolicina = (int)ostava["kolicina"];
+            rok_trajanja = ostava["rok_trajanja"].ToString();
+        }
+    }
+}
